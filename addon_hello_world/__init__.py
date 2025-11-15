def60d9c7395340c21800ea4d65d09e41e4c4c42
Pome <unclepome.dev@gmail.com>
2025-11-15 09:23:56 +0900

    hello addon

--- addon_hello_world/__init__.py
+++ addon_hello_world/__init__.py
@@ -1,1 +1,15 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from . import hello_operator, hello_ui_panel
+
+modules = (hello_operator, hello_ui_panel)
+
+
+def register():
+    for m in modules:
+        m.register()
+
+
+def unregister():
+    for m in reversed(modules):
+        m.unregister()
